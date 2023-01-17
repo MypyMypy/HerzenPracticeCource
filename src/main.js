dab6b1647de40a47e9c61a9f8543e02844d07fee
danich282@gmail.com <danich282@gmail.com>
2023-01-17 14:30:03 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import 'babel-polyfill';
+import './styles/main.css';
+import { Card } from './scripts/Card.js';
+
+// const container = document.createElement('div');
+// container.classList.add('container');
+// document.body.append(container)
+
+
+new Card(".container").init();
+
+
+
